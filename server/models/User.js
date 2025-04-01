21611359d18df14a41b2dbf1977202b1068f076d
Julie Cheng <jucheng925@gmail.com>
2025-04-01 04:11:56 -0400

    schema added, will need to add more once games figured out

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,24 @@
 import mongoose from 'mongoose';
 
+
 const userSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+
+}, {
+    timestamps: true // createdAt, updatedAt
 })
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
