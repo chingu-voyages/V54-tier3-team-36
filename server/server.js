eb3b16d345db20a30ecd996c253f83cf7da9e428
Julie Cheng <jucheng925@gmail.com>
2025-04-11 18:34:14 -0400

    add consent for signup

--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ import authRoutes from './routes/auth.js'
 import dashRoutes from './routes/dashboard.js'
 
 dotenv.config()
-console.log('Loaded env:', process.env.JWT_TOKEN_SECRET);
-
 
 const app = express();
 
