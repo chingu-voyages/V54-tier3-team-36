5d7cdd936f111da19a876bb17cb492249b877aea
Alvarado08 <jorgealvarado0238@gmail.com>
2025-04-12 01:12:54 -0500

    Card Flip Effect

--- client/src/components/AnimalFlashcardGame/FlashCard.jsx
+++ client/src/components/AnimalFlashcardGame/FlashCard.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import Tilt from "react-parallax-tilt";
+import ReactCardFlip from "react-card-flip";
+import AnimalWorld from "../../assets/AnimalWorld.png";
 
 export default function FlashCard({ image }) {
+  const [isFlipped, setIsFlipped] = useState(false);
   return (
     <Tilt>
-      <div className="rounded-md overflow-hidden w-48 sm:w-56 md:w-60 aspect-[2/3]">
-        <img
-          className="object-cover w-full h-full"
-          src={image}
-          alt="Animal image"
-        />
-      </div>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <div
+          key="front"
+          onClick={() => setIsFlipped(true)}
+          className="rounded-md overflow-hidden w-48 sm:w-56 md:w-60 aspect-[2/3] shadow-md flex items-center justify-center"
+        >
+          <img
+            className="align-middle size-52"
+            src={AnimalWorld}
+            alt="App Logo"
+          />
+        </div>
+        <div
+          key="back"
+          onClick={() => setIsFlipped(false)}
+          className="rounded-md overflow-hidden w-48 sm:w-56 md:w-60 aspect-[2/3]"
+        >
+          <img
+            className="object-cover w-full h-full"
+            src={image}
+            alt="Animal image"
+          />
+        </div>
+      </ReactCardFlip>
     </Tilt>
   );
 }
