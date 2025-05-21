54bdcb08dfa8dabafed2206b338cc084118a311a
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-05-21 17:20:03 -0300

    refactor(login): add icons, change styles

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import {useAuth} from '../context/auth'
-import {useNavigate} from 'react-router'
+import React from 'react';
+import {useNavigate} from 'react-router';
 import {useForm} from 'react-hook-form';
 import {zodResolver} from "@hookform/resolvers/zod";
+import {Lock, Mail} from 'lucide-react';
 import {loginSchema} from "../schemas/auth.schema";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form";
-import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
+import {useAuth} from '../context/auth';
 import {useToast} from "@/hooks/use-toast.js";
+import {Form, FormControl, FormField, FormItem, FormMessage,} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
 
 
 const Login = () => {
@@ -68,26 +69,31 @@ const Login = () => {
     }
 
     return (
-        <div
-            className='w-[95%] h-[70vh] bg-emerald-700 text-lg bg-opacity-75 rounded-[3vw] px-12 md:pt-12 mb-10 mx-auto mt-8'>
-            <div className="flex justify-center items-center mt-8 md:mt-12 px-4 sm:px-6 lg:px-0">
+        <div className="flex h-[80vh] items-center justify-center bg-emerald-700 bg-opacity-75 px-4 rounded-2xl">
+            <div
+                className="w-full max-w-md bg-white/20 backdrop-blur-sm border border-white/30 rounded-2xl p-6 sm:p-8 space-y-6">
+                <h2 className="text-2xl font-semibold text-white text-center">Login</h2>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)}
-                          className="w-full max-w-md space-y-6 bg-emerald-700 bg-opacity-75 p-8 rounded-2xl">
-                        <h2 className="text-3xl font-bold text-center">Animal World</h2>
-                        <p className="text-center italic">~Let's get back to the fun!~</p>
+                          className="flex flex-col gap-6">
                         <FormField
                             control={form.control}
                             name="email"
                             render={({field}) => (
                                 <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="email"
-                                            placeholder="email@example.com"
-                                            {...field} />
-                                    </FormControl>
+                                    <div className="relative">
+                                        <FormControl>
+                                            <Input
+                                                type="email"
+                                                placeholder="email@example.com"
+                                                className="w-full rounded-full py-3 px-4 pr-10 placeholder-white/70 text-white bg-transparent border border-transparent ring-1 ring-white/50 hover:ring-white focus:ring-2 focus:ring-teal-800 focus:outline-none transition"
+                                                {...field} />
+                                        </FormControl>
+                                        <Mail
+                                            size={20}
+                                            className="absolute right-3 top-1/2 -translate-y-1/2 text-white/70"
+                                        />
+                                    </div>
                                     <FormMessage/>
                                 </FormItem>
                             )}
@@ -97,21 +103,40 @@ const Login = () => {
                             name="password"
                             render={({field}) => (
                                 <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="password"
-                                            placeholder="********"
-                                            {...field}
+                                    <div className="relative">
+                                        <FormControl>
+                                            <Input
+                                                type="password"
+                                                placeholder="password"
+                                                className="w-full rounded-full py-3 px-4 pr-10 placeholder-white/70 text-white border border-transparent ring-1 ring-white/50 hover:ring-white focus:ring-2 focus:ring-teal-800 focus:outline-none transition"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <Lock
+                                            size={20}
+                                            className="absolute right-3 top-1/2 -translate-y-1/2 text-white/70"
                                         />
-                                    </FormControl>
+                                    </div>
                                     <FormMessage/>
                                 </FormItem>
                             )}
                         />
                         <Button type="submit"
-                                className="w-3/4 mx-auto block bg-teal-800 shadow-md shadow-teal-500/50 hover:opacity-85 rounded-full text-white ml-2 border-none">Log
-                            In</Button>
+                                className="w-full mt-8 rounded-full py-5 text-white bg-teal-800 shadow-md shadow-teal-500/50 border-none transition-opacity  hover:bg-teal-700">Submit</Button>
+
+                        <p className="text-center text-white text-sm">
+                            Don’t have an account?{' '}
+                            <a
+                                href="#"
+                                onClick={e => {
+                                    e.preventDefault()
+                                    navigate('/signup')
+                                }}
+                                className="text-white underline hover:opacity-80 transition"
+                            >
+                                Register
+                            </a>
+                        </p>
                     </form>
                 </Form>
             </div>
