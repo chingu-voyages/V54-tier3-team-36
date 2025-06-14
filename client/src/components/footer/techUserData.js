fa0738dd9ddfe26f66cbab9bc720953a416b118c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-06-14 20:16:32 -0300

    refactor(footer): add icons for tech stack

---
+++ client/src/components/footer/techUserData.js
@@ -0,0 +1,100 @@
+import reactImg from "../../assets/images/footer-images/react.png";
+import viteImg from "../../assets/images/footer-images/vite.png";
+import tailwindImg from "../../assets/images/footer-images/tailwind.png";
+import routerImg from "../../assets/images/footer-images/react-router.png";
+import zodImg from "../../assets/images/footer-images/zod.png";
+import axiosImg from "../../assets/images/footer-images/axios.png";
+import nodeImg from "../../assets/images/footer-images/node.png";
+import expressImg from "../../assets/images/footer-images/express.png";
+import mongoImg from "../../assets/images/footer-images/mongo.png";
+import jsImg from "../../assets/images/footer-images/javascript.png";
+import tsImg from "../../assets/images/footer-images/typescript.png";
+import gitImg from "../../assets/images/footer-images/git.png";
+
+
+export const techStack = [
+    {
+        name: "React",
+        image: reactImg,
+        alt: "React",
+        category: "frontend",
+        color: "cyan"
+    },
+    {
+        name: "Vite",
+        image: viteImg,
+        alt: "Vite",
+        category: "frontend",
+        color: "blue"
+    },
+    {
+        name: "Tailwind CSS",
+        image: tailwindImg,
+        alt: "Tailwind CSS",
+        category: "frontend",
+        color: "cyan"
+    },
+    {
+        name: "React Router",
+        image: routerImg,
+        alt: "React Router",
+        category: "frontend",
+        color: "orange"
+    },
+    {
+        name: "Zod",
+        image: zodImg,
+        alt: "Zod",
+        category: "frontend",
+        color: "orange"
+    },
+    {
+        name: "Axios",
+        image: axiosImg,
+        alt: "Axios",
+        category: "frontend",
+        color: "green"
+    },
+    {
+        name: "Node.js",
+        image: nodeImg,
+        alt: "Node.js",
+        category: "backend",
+        color: "green"
+    },
+    {
+        name: "Express",
+        image: expressImg,
+        alt: "Express.js",
+        category: "backend",
+        color: "gray"
+    },
+    {
+        name: "MongoDB",
+        image: mongoImg,
+        alt: "MongoDB",
+        category: "database",
+        color: "green"
+    },
+    {
+        name: "JavaScript",
+        image: jsImg,
+        alt: "JavaScript",
+        category: "language",
+        color: "yellow"
+    },
+    {
+        name: "TypeScript",
+        image: tsImg,
+        alt: "TypeScript",
+        category: "language",
+        color: "blue"
+    },
+    {
+        name: "Git",
+        image: gitImg,
+        alt: "GIT",
+        category: "version",
+        color: "blue"
+    },
+];
