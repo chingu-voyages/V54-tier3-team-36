ac31f06f21c8ed1e5124a67a273f1f3e29641e40
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-07-23 12:16:37 +0200

    Set up jest and jest-axe for unit testing that includes accessibility checks

---
+++ client/jest.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/fileMock.js"
+  }
+};
