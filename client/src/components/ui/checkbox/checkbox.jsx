819e8d7dcbb6bab108bd849afe1137c406b19856
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-07-23 12:17:49 +0200

    Test checkbox component to ensure the testing setup is correct

---
+++ client/src/components/ui/checkbox/checkbox.jsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
+import { Check } from "lucide-react"
+import { cn } from "../../../lib/utils"
+
+const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
+    <CheckboxPrimitive.Root
+        ref={ref}
+        className={cn(
+            "peer h-4 w-4 shrink-0 p-0 rounded-sm border border-white bg-transparent hover:border-teal-300 data-[state=checked]:border-transparent data-[state=checked]:bg-teal-800 transition-colors focus:outline-none focus:ring-1 focus:ring-teal-800",
+            className
+        )}
+        {...props}>
+        <CheckboxPrimitive.Indicator className={cn("flex items-center justify-center text-current")}>
+            <Check className="h-4 w-4" />
+        </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+))
+Checkbox.displayName = CheckboxPrimitive.Root.displayName
+
+export { Checkbox }
---
+++ client/src/components/ui/checkbox/checkbox.test.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { axe } from "jest-axe";
+import { Checkbox } from "./checkbox";
+import { it, describe, expect } from "@jest/globals";
+
+describe("Checkbox component", () => {
+  it("renders correctly and can be checked", () => {
+    render(<Checkbox data-testid="checkbox" />);
+    const checkbox = screen.getByTestId("checkbox");
+    expect(checkbox).not.toHaveAttribute("data-state", "checked");
+    fireEvent.click(checkbox);
+    expect(checkbox).toHaveAttribute("data-state", "checked");
+  });
+
+  it("is accessible with no a11y violations", async () => {
+    const { container } = render(<Checkbox aria-label="Accept terms" />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
