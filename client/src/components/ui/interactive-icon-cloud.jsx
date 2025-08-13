028d9261f46f2d08c58133d9368497016be9e1aa
aigul-ermak <aigul.erma@gmail.com>
2025-08-13 08:19:41 -0300

    added interactive library

---
+++ client/src/components/ui/interactive-icon-cloud.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useMemo, useState } from "react";
+import {
+  Cloud,
+  fetchSimpleIcons,
+  renderSimpleIcon,
+} from "react-icon-cloud";
+
+export const cloudProps = {
+  containerProps: {
+    style: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      paddingTop: 40,
+    },
+  },
+  options: {
+    reverse: true,
+    depth: 1,
+    wheelZoom: false,
+    imageScale: 2,
+    activeCursor: "default",
+    tooltip: "native",
+    initial: [0.1, -0.1],
+    clickToFront: 500,
+    tooltipDelay: 0,
+    outlineColour: "#0000",
+    maxSpeed: 0.04,
+    minSpeed: 0.02,
+  },
+};
+
+export const renderCustomIcon = (icon, isDark = false) => {
+  const bgHex = isDark ? "#080510" : "#f3f2ef";
+  const fallbackHex = isDark ? "#ffffff" : "#6e6e73";
+  const minContrastRatio = isDark ? 2 : 1.2;
+
+  return renderSimpleIcon({
+    icon,
+    bgHex,
+    fallbackHex,
+    minContrastRatio,
+    size: 42,
+    aProps: {
+      href: undefined,
+      target: undefined,
+      rel: undefined,
+      onClick: (e) => e.preventDefault(),
+    },
+  });
+};
+
+export function IconCloud({ iconSlugs, isDark = false }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchSimpleIcons({ slugs: iconSlugs }).then(setData);
+  }, [iconSlugs]);
+
+  const renderedIcons = useMemo(() => {
+    if (!data) return null;
+
+    return Object.values(data.simpleIcons).map((icon) =>
+      renderCustomIcon(icon, isDark)
+    );
+  }, [data, isDark]);
+
+  return (
+    <Cloud {...cloudProps}>
+      <>{renderedIcons}</>
+    </Cloud>
+  );
+}
